8b463374fda7bc25044e033766989d5a4d2a316c
sweelemk <sweelemk@gmail.com>
2016-05-30 22:55:37 +0300

    create index page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-	// body...
+	
+	//detected mobile an init fullpage slider
+	if(!head.mobile) {
+		initFullpage();
+	}
+
+	function initFullpage() {
+		$('.fullpage').fullpage({
+			css3: true,
+			easingcss3: 'cubic-bezier(1.000, 0.010, 0.990, 0.470)',
+			scrollingSpeed: 1000,
+			fitToSection: true
+		});
+	}
+
 })
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,6 @@
 //= lib/head.js
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/fullpage.js/jquery.fullPage.min.js
+//= lib/html5shiv.min.js
 
 //= app.js
