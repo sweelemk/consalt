e6327a3613a05b8e770c11644296c51f7422b0cc
sweelemk <sweelemk@gmail.com>
2016-06-02 20:52:21 +0300

    second slide and start menu

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -37,7 +37,76 @@ $(document).ready(function () {
 			fitToSection: true,
 			navigation: true,
 			navigationPosition: 'left',
+			afterLoad: function(anchorLink, index){
+				if(index === 1) {
+					$('.menu-left').removeClass('hidden');
+				} else {
+					$('.menu-left').addClass('hidden');
+				}
+			},
+			onLeave: function(index, newIndex, direction){
+				if(newIndex === 1) {
+					$('.menu-left').removeClass('hidden');
+				} else {
+					$('.menu-left').addClass('hidden');
+				}
+			}
 		});
-	}
+	};
+
+	//menu toggle
+	function menuToggle() {
+		var menu = $('.menu'),
+			overlay = $('.menu-overlay'),
+			Lside = overlay.find('.menu__left');
+
+			menu.on('click', function(){
+				if(overlay.hasClass('menu-open')) {
+					overlay
+						.removeClass('menu-open')
+						.delay(500)
+						.fadeOut(0);
+				} else {
+					overlay.fadeIn({
+						duration: 0,
+						complete: function(){
+							overlay.addClass('menu-open');
+						}
+					})
+				}
+			});
+			Lside.on('click', function(){
+				overlay
+					.removeClass('menu-open')
+					.delay(5000)
+					.fadeOut(0);
+			});
+
+	} menuToggle();
+
+	// switcher
+	function switcher(){
+		var bContainer = $('.business'),
+			bItem = bContainer.find('.business-item'),
+			hContainer = $('.head-categories'),
+			hCmain = hContainer.prev('.head'),
+			hItem = hContainer.find('.head-categories__item');
+
+		bItem.on('mouseenter', function(){
+			var index = $(this).index();
+
+			hCmain.addClass('hidden');
+			hContainer.addClass('visible');
+			hItem.eq(index).addClass('active').siblings().removeClass('active');
+
+		});
+
+		bItem.on('mouseleave', function(){
+			hCmain.removeClass('hidden');
+			hContainer.removeClass('visible');
+			hItem.removeClass('active');
+		});
+		
+	} switcher();
 
 })
